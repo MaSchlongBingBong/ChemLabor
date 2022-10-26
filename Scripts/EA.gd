51f891eff7f319cfbc9afa1f16d3b0ca69a5d84c
LichtMarv <mvahldiek2@gmail.com>
2022-10-26 15:22:23 +0200

    basic sphere spawn

--- Scripts/EA.gd
+++ Scripts/EA.gd
@@ -8,7 +8,13 @@ extends Spatial
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var mesh_node = MeshInstance.new()
+	var sphere_mesh = SphereMesh.new()
+	sphere_mesh.radius = 0.5
+	sphere_mesh.height = 1
+	mesh_node.mesh = sphere_mesh
+	add_child(mesh_node)
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
