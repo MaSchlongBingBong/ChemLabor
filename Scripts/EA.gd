ef589d0d0af0f5a2dca4162750c6c924b2f604dd
LichtMarv <mvahldiek2@gmail.com>
2022-10-26 15:36:30 +0200

    modulized spawning

--- Scripts/EA.gd
+++ Scripts/EA.gd
@@ -8,14 +8,19 @@ extends Spatial
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	spawnSphere(1, Vector3(1,0,0))
+	spawnSphere(0.5, Vector3(1.5,1,0))
+	spawnSphere(0.5, Vector3(1.5,-1,0))
+
+
+func spawnSphere(radius: float, pos: Vector3):
 	var mesh_node = MeshInstance.new()
 	var sphere_mesh = SphereMesh.new()
-	sphere_mesh.radius = 0.5
-	sphere_mesh.height = 1
+	sphere_mesh.radius = radius
+	sphere_mesh.height = radius*2
 	mesh_node.mesh = sphere_mesh
 	add_child(mesh_node)
-
-
+	mesh_node.translation = pos
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
