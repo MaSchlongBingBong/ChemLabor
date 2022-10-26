e4923ed860320501a994db6772d7651f36567e4e
LichtMarv <mvahldiek2@gmail.com>
2022-10-26 19:38:28 +0200

    fixed burner

--- Scripts/EA.gd
+++ Scripts/EA.gd
@@ -1,6 +1,30 @@
 extends Spatial
 
 
+var mol = {
+	atoms = [
+		{
+			name = "Carbon",
+			pos = {x=1, y=0, z=0},
+			scale = 1
+		},
+		{
+			name = "Carbon",
+			pos = {x=-1, y=0, z=0},
+			scale = 1
+		}
+	],
+	bonds = [
+		{
+			from = 0,
+			to = 1,
+			count = 2
+		}
+	]
+}
+
+var atomObjects = []
+var bondObjects = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,15 +34,69 @@ func _ready():
 	var data = file.get_as_text()
 	var res = JSON.parse(data).result
 	print(res["name"])
+	
+	generate_molecule()
 
-	for step in res["steps"]:
-		print(step)
-		pass
 
-	spawnSphere(1, Vector3(1,0,0))
-	spawnSphere(0.5, Vector3(1.5,1,0))
-	spawnSphere(0.5, Vector3(1.5,-1,0))
+# Note: passing a value for the type parameter causes a crash
+static func get_child_of_type(node: Node, child_type):
+	for i in range(node.get_child_count()):
+		var child = node.get_child(i)
+		if child is child_type:
+			return child
 
+
+# Note: passing a value for the type parameter causes a crash
+static func get_children_of_type(node: Node, child_type):
+	var list = []
+	for i in range(node.get_child_count()):
+		var child = node.get_child(i)
+		if child is child_type:
+			list.append(child)
+	return list
+
+func generate_molecule():
+	atomObjects = [];
+	bondObjects = [];
+
+	for child in get_children():
+		child.queue_free()
+	
+
+	for i in range(len(mol["atoms"])):
+		var atom = mol["atoms"][i];
+		var ao = spawnSphere(atom.scale, Vector3(atom.pos.x,atom.pos.y,atom.pos.z))
+		ao.name = atom.name + " [" + str(i) + "]";
+		var m = SpatialMaterial.new()
+		m.albedo_color = Color( 1, 0, 1, 1 )
+		ao.set_surface_material(0,m)
+		#get_child_of_type(ao,MeshInstance)
+		#atomObjects[i] = ao;
+
+	#foreach (Bond bond in bonds)
+	# for (int b = 0; b < bonds.Length; b++)
+	# {
+	# 	Bond bond = bonds[b];
+	# 	for (int i = 1; i <= bond.number; i++)
+	# 	{
+	# 		Atom from = atoms[bond.from];
+	# 		Atom to = atoms[bond.to];
+	# 		atoms[bond.from].AddBond(b);
+	# 		atoms[bond.to].AddBond(b);
+	# 		GameObject bo = Instantiate(bondObject);
+	# 		bo.transform.SetParent(transform);
+	# 		bo.name = from.name + " - " + to.name + " [" + b + "," + i + "]";
+	# 		bo.transform.localPosition = bond.number > 1 ? from.pos + (new Vector3(0,0.1f * (i*2 - 3),0)) : from.pos;
+	# 		//bo.transform.LookAt(to.pos + transform.position);
+	# 		bo.transform.forward = atomObjects[bond.to].transform.position - atomObjects[bond.from].transform.position;
+	# 		float dist = (to.pos - from.pos).magnitude/2;
+	# 		float curr = 0.1f * (from.scale + to.scale)/2;
+	# 		bo.transform.localScale = new Vector3(curr,curr,dist);
+	# 		bo.GetComponentInChildren<MeshRenderer>().material = mats.GetValueOrDefault(to.name);
+	# 		///TODO: will overwrite double bonds .... pls fix
+	# 		bondObjects[b] = bo;
+	# 	}
+	# }
 
 func spawnSphere(radius: float, pos: Vector3):
 	var mesh_node = MeshInstance.new()
@@ -28,6 +106,7 @@ func spawnSphere(radius: float, pos: Vector3):
 	mesh_node.mesh = sphere_mesh
 	add_child(mesh_node)
 	mesh_node.translation = pos
+	return mesh_node
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
