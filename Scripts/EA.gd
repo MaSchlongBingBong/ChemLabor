e8a1a39c7375480732365e38ad846013b6e43adb
LichtMarv <mvahldiek2@gmail.com>
2022-10-28 21:05:09 +0200

    midway through doing the animation lol

--- Scripts/EA.gd
+++ Scripts/EA.gd
@@ -1,85 +1,115 @@
 extends "res://addons/godot-xr-tools/objects/pickable.gd"
 
 
-var mol = {
-	atoms = [
-		{
-			name = "Carbon",
-			pos = {x=1, y=0, z=0},
-			scale = 1
-		},
-		{
-			name = "Carbon",
-			pos = {x=-1, y=0, z=0},
-			scale = 1
-		},
-		{
-			name = "Hydrogen",
-			pos = {x=1.5,y=1,z=0},
-			scale=0.5
-		},
-		{
-			name = "Hydrogen",
-			pos = {x=1.5,y=-1,z=0},
-			scale=0.5
-		},
-		{
-			name = "Hydrogen",
-			pos = {x=-1.5,y=1,z=0},
-			scale=0.5
-		},
-		{
-			name = "Hydrogen",
-			pos = {x=-1.5,y=-1,z=0},
-			scale=0.5
-		}
-	],
-	bonds = [
-		{
-			from = 0,
-			to = 1,
-			number = 2
-		},
-		{
-			from = 0,
-			to = 2,
-			number = 1
-		},
-		{
-			from = 0,
-			to = 3,
-			number = 1
-		},
-		{
-			from = 1,
-			to = 4,
-			number = 1
-		},
-		{
-			from = 1,
-			to = 5,
-			number = 1
-		},
-	]
-}
+var mol = {}
+var steps = {}
 
 var atomObjects = []
 var bondObjects = []
-	
+var currentMoveableAtom = {}
+
+var currIndex = -1
+var counter = 0
+var currTimeout = 0
 
 export(Dictionary) var atom_colors;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	loadMolecule()
+	generate_molecule()
+	playAnim()
+
+func action():
+	playAnim()
+
+func loadMolecule():
 	var file = File.new()
 	if file.open("res://Scripts/test.json", File.READ) != OK:
 		return
 	var data = file.get_as_text()
 	var res = JSON.parse(data).result
 	print(res["name"])
-	
-	generate_molecule()
+	mol = res.mol
+	steps = res.steps
 
+func playAnim():
+	loadMolecule()
+	generate_molecule()
+	currIndex = 0
+
+
+func nextAnimStep(delta):
+	if currIndex == -1:
+		return
+	counter += delta;
+
+	if counter < currTimeout:
+		return
+
+	var step = steps[currIndex]
+	match step.type:
+		"Nop":
+			pass
+		"createBond":
+			mol.bonds.append({from=step.data.from, to=step.data.to, number=step.data.get("number",1)})
+			generate_molecule()
+		"deleteBond":
+			var bond = mol.bonds[step.data.idx]
+			if bond.number > 1:
+				mol.bonds[step.data.idx].number -= 1
+			else:
+				mol.bonds.remove(step.data.idx)
+			generate_molecule()
+		"playAudio":
+				print("playing audio once implemented lol")
+		"moveAtom":
+				moveAtom(step.data.idx, dict2vec(step.data.pos), step.duration)
+		_:
+			printerr("type '" + step.type + "' unknown")
+		
+	counter = 0;
+	currTimeout = step.duration;
+	currIndex += 1;
+	print(currIndex);
+	if currIndex >= len(steps):
+		currIndex = -1;
+
+
+func nextDrawStep(delta):
+	if !currentMoveableAtom.empty():
+		currentMoveableAtom.timer += delta;
+		# Move Atom
+		currentMoveableAtom.atomObject.translation = lerp(currentMoveableAtom.from, currentMoveableAtom.to, currentMoveableAtom.timer/currentMoveableAtom.time);
+		
+		#Move Bonds
+		for i in range(len(currentMoveableAtom.bondObjects)):
+			var bo = currentMoveableAtom.bondObjects[i];
+			var from = bo.translation;
+			var to = currentMoveableAtom.atomObject.translation;
+			#bo.transform.LookAt(to.pos + transform.position);
+			var dir = currentMoveableAtom.atomObject.global_translation - bo.global_translation
+			if Vector3.UP.cross(dir) != Vector3():
+				bo.look_at(currentMoveableAtom.atomObject.global_translation, Vector3.UP);
+			else:
+				bo.rotate_x(sign(dir.y) * PI/2)
+			
+			var dist = (to - from).length();
+			var curr = bo.scale.x;
+			bo.translate(Vector3(0,0,-dist/2));
+			bo.rotate_object_local(Vector3(1,0,0),PI/2)
+
+			
+			##bo.transform.localScale = Vector3(curr,curr,dist);
+
+		if currentMoveableAtom.timer >= currentMoveableAtom.time:
+			currentMoveableAtom = {};
+			generate_molecule();
+
+func _process(delta):
+	nextDrawStep(delta)
+	nextAnimStep(delta)
+	
 
 # Note: passing a value for the type parameter causes a crash
 static func get_child_of_type(node: Node, child_type):
@@ -98,6 +128,23 @@ static func get_children_of_type(node: Node, child_type):
 			list.append(child)
 	return list
 
+func moveAtom(atomIndex: int, pos: Vector3, time: float):
+	var atom = mol.atoms[atomIndex];
+	var bonds =  [];
+	for idx in atom.bonds:
+		bonds.append(bondObjects[idx]);
+
+	currentMoveableAtom = {
+		from = dict2vec(atom.pos),
+		to = pos,
+		time = time,
+		timer = 0,
+		bondObjects = bonds,
+		atomObject = atomObjects[atomIndex],
+	};
+
+	mol.atoms[atomIndex].pos = pos;
+
 func generate_molecule():
 	atomObjects = [];
 	atomObjects.resize(len(mol.atoms))
@@ -110,6 +157,7 @@ func generate_molecule():
 
 	for i in range(len(mol["atoms"])):
 		var atom = mol["atoms"][i];
+		mol.atoms[i]["bonds"] = []
 		var ao = spawnSphere(atom.scale, Vector3(atom.pos.x,atom.pos.y,atom.pos.z))
 		ao.name = atom.name + " [" + str(i) + "]";
 		var m = SpatialMaterial.new()
@@ -122,8 +170,8 @@ func generate_molecule():
 		for i in range(bond.number):
 			var from = mol.atoms[bond.from];
 			var to = mol.atoms[bond.to];
-			# atoms[bond.from].AddBond(b);
-			# atoms[bond.to].AddBond(b);
+			mol.atoms[bond.from].bonds.append(b);
+			mol.atoms[bond.to].bonds.append(b);
 			var curr = 0.1 * (from.scale + to.scale)/2;
 			var dist = (dict2vec(to.pos) - dict2vec(from.pos)).length();
 			var bo = spawnCylinder(curr,dist)
@@ -131,7 +179,11 @@ func generate_molecule():
 			#bo.transform.localPosition = bond.number > 1 ? from.pos + (new Vector3(0,0.1f * (i*2 - 3),0)) : from.pos;
 			var trans = dict2vec(from.pos)
 			bo.translation = trans
-			bo.look_at(atomObjects[bond.to].global_translation, Vector3.UP);
+			var dir = atomObjects[bond.to].global_translation - bo.global_translation
+			if Vector3.UP.cross(dir) != Vector3():
+				bo.look_at(atomObjects[bond.to].global_translation, Vector3.UP);
+			else:
+				bo.rotate_x(sign(dir.y) * PI/2)
 			if bond.number > 1:
 				trans = trans + (Vector3(0,0.1 * ((i+1)*2 - 3),0))
 			bo.translation = trans
