57bab8d4c3745f6789813ef12c9a131e34dc2587
LichtMarv <mvahldiek2@gmail.com>
2022-10-31 16:01:01 +0100

    created settings board

---
+++ Scripts/SettingsBoard.gd
@@ -0,0 +1,24 @@
+extends Spatial
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_AudioButton_pressed_on():
+	AudioServer.set_bus_mute(0,false)
+
+
+func _on_AudioButton_pressed_off():
+	AudioServer.set_bus_mute(0,true)
--- Scripts/resetButton.gd
+++ Scripts/resetButton.gd
@@ -5,26 +5,49 @@ extends Spatial
 # var a = 2
 # var b = "text"
 
-signal reset
-var pressed: bool
+signal pressed_on
+signal pressed_off
+
+var pressed: bool = false
+export var state: bool = false
+export var toggle: bool = false
+var button_mesh
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Global.Instance.data["reset_button"] = self
-
-func _pressed():
-	pass
+	button_mesh = $Button/CollisionShape/MeshInstance
+	connect("pressed_on", self, "pressedOn")
+	connect("pressed_off", self, "pressedOff")
+	if state:
+		pressedOn()
+		emit_signal("pressed_on")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
+func pressedOn():
+	button_mesh.get_active_material(0).albedo_color = Color(0,1,0,1)
+	button_mesh.get_active_material(0).emission = Color(0,1,0,1)
+
+func pressedOff():
+	button_mesh.get_active_material(0).albedo_color = Color(1,0,0,1)
+	button_mesh.get_active_material(0).emission = Color(1,0,0,1)
+
 
 func _on_Area_body_entered(_body):
-	if not pressed:
-		emit_signal("reset")
-		pressed = true
+	if toggle:
+		state = not state
+		if state:
+			emit_signal("pressed_on")
+		else:
+			emit_signal("pressed_off")
+	elif not pressed:
+		emit_signal("pressed_on")
+	pressed = true
 
 
 func _on_Area_body_exited(_body):
+	if not toggle:
+		emit_signal("pressed_off")
 	pressed = false
