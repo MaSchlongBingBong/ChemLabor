1cba8c370561c3ba76c362921bd79f0d4a2e6c2e
MaSchlongBingBong <thgeren@gmail.com>
2022-11-08 21:57:31 +0100

    Added flowingLiquid and EthenParticle

--- Scripts/CopperPlate.gd
+++ Scripts/CopperPlate.gd
@@ -4,17 +4,12 @@ extends "res://addons/godot-xr-tools/objects/pickable.gd"
 # var a = 2
 # var b = "text"
 
-static func get_child_of_type(node: Node, child_type):
-	for i in range(node.get_child_count()):
-		var child = node.get_child(i)
-		if child is child_type:
-			return child
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func changeColor(copperoxid):
-	var mesh = get_child_of_type(self,MeshInstance)
+	var mesh = Global.get_child_of_type(self,MeshInstance)
 	mesh.set_surface_material(0, copperoxid)
 
 
--- Scripts/Pressure&Wash.gd
+++ Scripts/Pressure&Wash.gd
@@ -24,85 +24,21 @@ var isFlowing
 var ethen
 var dbe
 var counter = 0
-var texture = load("res://Materials/BromideWater.tres")
-var scaleTexture = load("res://Materials/DibromEthen.res")
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	ethen = createEthen()
+	ethen = Global.loadScene(self, load("res://Scene/EthenParticals.tscn"))
+	ethen.name = "Ethen"	
+	dbe = Global.loadScene(self, load("res://Scene/flowingLiquid.tscn"))
+	dbe.name = "Dbe"
 	# Replace with function body.
-
-
-func createEthen():
-	var par = Particles.new()
-	var mesh = CubeMesh.new()
-	mesh.size = Cscale
-	par.draw_passes = drawPasses
-	par.draw_pass_1 = mesh
-	var mat = ParticlesMaterial.new()
-	par.amount = amount
-	par.lifetime = lifetime
-	par.local_coords = localcords
-	par.draw_order = drawOrder
-	mat.emission_shape = emissionShape
-	mat.emission_sphere_radius = emissionSphereRadius
-	mat.direction = direction
-	mat.gravity = gravity
-	mat.linear_accel = accel
-	mat.linear_accel_random = accelRnd
-	mat.scale = Pscale
-	par.process_material = mat
-	var Ethen = Spatial.new()
-	add_child(Ethen)
-	Ethen.scale = Escale
-	Ethen.add_child(par)
-	return Ethen
-
-func createDiBromEthen():
-	var par = Particles.new()
-	var mesh = CubeMesh.new()
-	var mat = ParticlesMaterial.new()
-	par.amount = 20
-	par.lifetime = 0.75
-	par.draw_passes = drawPasses
-	par.draw_pass_1 = mesh
-	mat.linear_accel = 100
-	mat.scale_curve = scaleTexture
-	par.process_material = mat
-	par.scale = Vector3(0.01, 0.01, 0.01)
-	var Dbe = Spatial.new()
-	Dbe.name = "Dibromethen"
-	Dbe.scale = Vector3(0.6, 0.16, 0.6)
-	Dbe.translation = Vector3(-0.04, 0.05, 0.171)
-	add_child(Dbe)
-	Dbe.add_child(par)
-	return Dbe
-
 func _onPressed(body:Node):
 	if isFlowing == false:
 		if body.is_in_group("Hands"):
-			ethen = createEthen()
 			isFlowing = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	counter += delta
-	if ethen != null:
-		texture.albedo_color.a = lerp(1, 0, counter/time)
-		if texture.albedo_color.a <= 0.25:
-			ethen.queue_free()
-			ethen = null
-			print("Ethen:Done")
-	if dbe != null:
-		var mesh = Global.get_children_of_type(self, MeshInstance)[1]
-		var yStart = 0.025
-		var yEnd = 0.006
-		mesh = Global.lerpLiquid(mesh, yStart, yEnd, counter/time)
-		dbe.queue_free()
-		dbe = null
-		print(mesh.scale.y)
-		print("Scale:Done")
 
 func _onButtonPressed(body:Node):
-	if body.is_in_group("Hands"):
-		if ethen == null && isFlowing:
-			dbe = createDiBromEthen()
+	pass
