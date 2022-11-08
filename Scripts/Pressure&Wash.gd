807f234eaabb835c817d4f3a200b9392f71eb6f0
LichtMarv <mvahldiek2@gmail.com>
2022-11-08 23:29:12 +0100

    fixed syntax errors

--- Scripts/Pressure&Wash.gd
+++ Scripts/Pressure&Wash.gd
@@ -6,23 +6,25 @@ export var time:float = 10
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var isFlowing
+var isFlowing = false
 var ethen
 var dbe
 var counter = 0
-var liquid = Global.get_children_of_type(self, MeshInstance)[1]
+var liquid
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	liquid = Global.get_children_of_type(self, MeshInstance)[0]
 	
 	# Replace with function body.
 func _onPressed(body:Node):
+	print("help")
+	print(body.get_groups())
 	if body.is_in_group("Hands"):
 		if isFlowing == false:
 			ethen = Global.loadScene(self, load("res://Scene/EthenParticals.tscn"))
-			var newA = lerp(liquid.material[0].a, 0, counter)
-			if liquid.material[0].a >= 0.25:
-				liquid.material = newA
+			var newA = lerp(liquid.get_surface_material(0).albedo_color.a, 0, counter)
+			if liquid.get_surface_material(0).albedo_color.a >= 0.25:
+				liquid.get_surface_material(0).albedo_color.a = newA
 			isFlowing = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
