a7bd362484a1b11dd96766061a27bc1e3716978a
MaSchlongBingBong <thgeren@gmail.com>
2022-11-08 22:43:17 +0100

    Fixed Liquid shit

--- Scripts/Cup.gd
+++ Scripts/Cup.gd
@@ -14,6 +14,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	counter += delta
-	if !isFilled:
-		pass
 		# fillCup(liquid)
+
+
--- Scripts/Pressure&Wash.gd
+++ Scripts/Pressure&Wash.gd
@@ -1,19 +1,5 @@
 extends "res://addons/godot-xr-tools/objects/pickable.gd"
 
-export var amount:int = 100
-export var lifetime:int = 1
-export var localcords:bool = false
-export var drawOrder:int = 1
-export var emissionShape:int = 1
-export var emissionSphereRadius:float = 0.05
-export var direction:Vector3 = Vector3(0,-1,0)
-export var gravity: Vector3 = Vector3(0,-4,0)
-export var accel:float = 60.0
-export var accelRnd:float= 1.0 
-export var drawPasses:int = 1
-export var Pscale:float = 0.02
-export var Escale:Vector3 = Vector3(1,1,1)
-export var Cscale:Vector3 = Vector3(0.5,0.5,0.5)
 export var time:float = 10 
 
 
@@ -24,6 +10,7 @@ var isFlowing
 var ethen
 var dbe
 var counter = 0
+var liquid = Global.get_children_of_type(self, MeshInstance)[1]
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	ethen = Global.loadScene(self, load("res://Scene/EthenParticals.tscn"))
@@ -34,11 +21,18 @@ func _ready():
 func _onPressed(body:Node):
 	if isFlowing == false:
 		if body.is_in_group("Hands"):
+			liquid.material[0].a
 			isFlowing = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	counter += delta
+	
 
 func _onButtonPressed(body:Node):
-	pass
+	if body.is_in_group("Hands"):
+		if dbe != null:
+			pass
+		elif dbe == null:
+			dbe = Global.loadScene(self, load("res://Scene/flowingLiquid.tscn"))
+			liquid = Global.lerpLiquid(liquid, 0.025, 0.003, counter/time)
