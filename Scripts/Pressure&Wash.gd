78a720051932d8f80dd34ab544c93804d26c818b
MaSchlongBingBong <thgeren@gmail.com>
2022-11-09 12:03:48 +0100

    FIXED: Pressure&Wash.gd; for the love of god pls let it be the last time

--- Scripts/Pressure&Wash.gd
+++ Scripts/Pressure&Wash.gd
@@ -1,40 +1,43 @@
 extends "res://addons/godot-xr-tools/objects/pickable.gd"
 
-export var time:float = 10 
-
-
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-var isFlowing = false
+var isFlowing
 var ethen
 var dbe
 var counter = 0
 var liquid
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	liquid = Global.get_children_of_type(self, MeshInstance)[0]
-	
+	liquid = Global.get_children_of_type(self, MeshInstance)[1]	
+	isFlowing = false
 	# Replace with function body.
 func _onPressed(body:Node):
 	print("help")
 	print(body.get_groups())
 	if body.is_in_group("Hands"):
-		if isFlowing == false:
+		while !isFlowing:
 			ethen = Global.loadScene(self, load("res://Scene/EthenParticals.tscn"))
 			var newA = lerp(liquid.get_surface_material(0).albedo_color.a, 0, counter)
-			if liquid.get_surface_material(0).albedo_color.a >= 0.25:
+			if liquid.get_surface_material(0).albedo_color.a > 0.1:
+				print(newA)
 				liquid.get_surface_material(0).albedo_color.a = newA
-			isFlowing = true
+				print("Applied new A")
+			else:
+				print("Done")
+				ethen.queue_free()
+				isFlowing = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	counter = delta
-	
+
 func _onButtonPressed(body:Node):
 	if body.is_in_group("Hands"):
-		if dbe != null:
-			pass
-		elif dbe == null:
+		if dbe == null:
 			dbe = Global.loadScene(self, load("res://Scene/flowingLiquid.tscn"))
-			liquid = Global.scaleLiquid(liquid,0.003, counter)
+			liquid = Global.scaleLiquid(liquid,0.01, counter/5)
+		else:
+			dbe.queue_free()
+
