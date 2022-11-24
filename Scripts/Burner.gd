8c065845e9e6a034e86ae96c99edc2e45433e4c3
MaSchlongBingBong <thgeren@gmail.com>
2022-11-24 18:55:43 +0100

    have done some real shit

--- Scripts/Burner.gd
+++ Scripts/Burner.gd
@@ -5,16 +5,21 @@ extends "res://addons/godot-xr-tools/objects/pickable.gd"
 # var a = 2
 # var b = "text"
 var copperoxid = load("res://Materials/Copperoxid.material")
-var copperMat;
+var copperMat
+var copper
 var mat
+var area
 # Called when the node enters the scene tree for the first time.
 var fire
+var timer = 10
+
 func _ready():
+	area = Global.get_child_of_type(self, Area)
 	fire = Global.loadScene(self, load("res://Scene/Fire.tscn")).get_child(0)
 	mat = fire.process_material
 	fire.scale.y = 0.25 
 	fire.translation.y = 0.19
-	fire.emitting = false
+	fire.emitting = true
 	# Replace with function body.
 
 func action():
@@ -28,14 +33,13 @@ func _onContact(body:Node):
 		copperMat = Global.get_child_of_type(body, MeshInstance).get_active_material(0)
 		if copperMat == copperoxid:
 			pass
-		elif fire:
+		elif fire.emitting:
 			mat.color = Color("#0e7310")
 			fire.process_material = mat
 			print("contact")
-			yield(get_tree().create_timer(10.0), "timeout")
-			body.changeColor(copperoxid)
 			mat.color = Color("#1624ef")
 			fire.process_material = mat
+
 
 
 func _onExit(body:Node):
@@ -46,5 +50,10 @@ func _onExit(body:Node):
 		fire.process_material = mat
 		print("exit")
 
-
+func _process(delta):
+	if fire.emitting:
+		for body in area.get_overlapping_bodies():
+			print(body)
+			if body.has_method("oxidize"):
+				body.call("oxidize", delta, copperoxid)
 
--- Scripts/CopperPlate.gd
+++ Scripts/CopperPlate.gd
@@ -3,7 +3,7 @@ extends "res://addons/godot-xr-tools/objects/pickable.gd"
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var timer = 10
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -12,8 +12,11 @@ func changeColor(copperoxid):
 	var mesh = Global.get_child_of_type(self,MeshInstance)
 	mesh.set_surface_material(0, copperoxid)
 
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+# func _process(delta):
+# 	pass
+func oxidize(delta, mat):
+	print("now oxidzing biaaatch")
+	timer -= delta
+	if timer <= 0:
+		changeColor(mat)
